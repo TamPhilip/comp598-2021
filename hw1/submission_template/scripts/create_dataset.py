2d5b0cc254a8790241d0c16d99aac294fedefefa
Philip Tam <philip.tam.code@gmail.com>
2021-09-16 19:32:53 -0400

    Completed HW1

---
+++ hw1/submission_template/scripts/create_dataset.py
@@ -0,0 +1,34 @@
+from pathlib import Path
+import os, sys
+parentdir = Path(__file__).parents[0]
+sys.path.append(parentdir)
+import pandas as pd
+
+
+class Dataset():
+
+    def __init__(self) -> None:
+        self.path = '/Users/philiptam/Projects/comp598-2021/hw1/submission_template/data/IRAhandle_tweets_1.csv'
+
+    def main(self):
+        df = self.create()
+        self.export(df)
+
+    def create(self):
+        df = pd.read_csv(self.path)
+        # Keep only first 10,000
+        df = df.head(10000)
+        # Keep only english
+        df = df[df['language'] == "English"]
+        # Remove rows with ?
+        df = df[df['content'].str.contains('\?') == False]
+        # Add Trump Feature
+        df['trump_mention'] = df['content'].str.contains('[^a-zA-Z:]Trump[^a-zA-Z:]|^Trump[^a-zA-Z:]')
+        return df
+
+    def export(self, df):
+        df[['tweet_id', 'publish_date', 'content','trump_mention']].to_csv('dataset.tsv', sep = '\t', index=False)
+        
+
+if __name__ == '__main__':
+    Dataset().main()
---
+++ hw1/submission_template/scripts/create_results.py
@@ -0,0 +1,32 @@
+from pathlib import Path
+import os, sys
+
+from pandas.core.frame import DataFrame
+parentdir = Path(__file__).parents[0]
+sys.path.append(parentdir)
+import pandas as pd
+
+
+class Result():
+
+    def __init__(self) -> None:
+        self.path = '/Users/philiptam/Projects/comp598-2021/hw1/submission_template/dataset.tsv'
+
+    def main(self):
+        df = self.create()
+        self.export(df)
+
+    def create(self):
+        df = pd.read_csv(self.path, sep='\t')
+        
+        data = {"result" : ["frac-trump-mentions"], "value" : [len(df[df['trump_mention'] == True]) / len(df)]}
+        new_df = DataFrame(data=data)
+
+        return new_df
+
+    def export(self, df):
+        df.to_csv('results.tsv', sep = '\t', index=False)
+        
+
+if __name__ == '__main__':
+    Result().main()
